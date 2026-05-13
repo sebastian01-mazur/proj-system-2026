5c0f59c8c99c8e790c483b9dd06f04042c75e3d1
Gajav <gabrys.jaw@gmail.com>
2026-05-13 06:25:32 +0200

    Test brancha feature/trip

--- backend/src/main/java/com/splittrip/trip/Trip.java
+++ backend/src/main/java/com/splittrip/trip/Trip.java
@@ -1,6 +1,7 @@
 package com.splittrip.trip;
 
 public class Trip {
+        //test nowego brancha
 
     //Centralna encja podróży (podroze), przechowująca budżet planowany, ramy czasowe, kraj docelowy, walutę bazową oraz status wyjazdu
 
